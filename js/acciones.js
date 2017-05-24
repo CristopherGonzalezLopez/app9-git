bd8ed8eac35e9ad5b4d5e9fc66710d439b888cdc
CristopherGonzalezLopez <gcristopher326@gmail.com>
2017-05-24 09:38:38 -0500

    subida1

--- js/acciones.js
+++ js/acciones.js
@@ -28,14 +28,14 @@
 	  
 	  function onSuccess(position){
 		  
-		  alert('Latitude:'     +position.coords.latitude     +'\n'+
-		  'Longitude:'         +position.coords.longitude    +'\n'+
-		  'Altitude:'          +position.coords.altitude    +'\n'+
-		  'Accuracy:'         +position.coords.accuracy    +'\n'+
-	      'Altitude Accuracy:'         +position.coords.altitudeAccuracy  +'\n'+
-		  'Heading:'         +position.coords.heading    +'\n'+
-		  'Speed:'         +position.coords.speed   +'\n'+
-		  'Timestamp:'         +position.timestamp    +'\n');
+		  alert('Latitude:'     + position.coords.latitude     +'\n'+
+		  'Longitude:'         + position.coords.longitude    +'\n'+
+		  'Altitude:'          + position.coords.altitude    +'\n'+
+		  'Accuracy:'         + position.coords.accuracy    +'\n'+
+	      'Altitude Accuracy:'         + position.coords.altitudeAccuracy  +'\n'+
+		  'Heading:'         + position.coords.heading    +'\n'+
+		  'Speed:'         + position.coords.speed   +'\n'+
+		  'Timestamp:'         + position.timestamp    +'\n');
 	  };
 	  
 	  function onError(error){
@@ -46,7 +46,7 @@
   function watchPosition(){
 	 
   var options={
-	  maximunAge:3600000,
+	  maximumAge:3600000,
 	  timeout:3000,
 	  enableHighAccuracy:true,
   }
@@ -60,7 +60,7 @@ $('#longitud').html(position.coords.longitude);
 $('#altitud').html(position.coords.altitude);
 $('#accuracy').html(position.coords.accuracy);
 $('#aaccuracy').html(position.coords.altitudeAccuracy);
-$('#heading').html(position.coords.heading);
+$('#headingg').html(position.coords.heading);
 $('#speed').html(position.coords.speed);
 $('#timestamp').html(position.timestamp);
   };
